cecadbd181cac8a2dce4aa48b9d3ba8d5e839f2a
damujuri <deva.amujuri@gmail.com>
2021-10-21 23:04:25 +0530

    second commit

---
+++ src/main/java/com/dsp/inmar/InMarConstants.java
@@ -0,0 +1,7 @@
+package com.dsp.inmar;
+
+public class InMarConstants {
+    public static final String CSV = "csv";
+    public static final String JSON = "json";
+    public static final String TXT = "txt";
+}
